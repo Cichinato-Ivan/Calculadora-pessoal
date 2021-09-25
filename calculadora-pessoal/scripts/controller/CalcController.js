10d3d4e3ac68fd0be5d3fde5cc605e3888a921a3
Ivan Cichinato de Oliveira <ivan.oliveira01@yahoo.com>
2021-09-26 00:25:17 +0100

    aplicando padrao MVC

--- calculadora-pessoal/scripts/controller/CalcController.js
+++ calculadora-pessoal/scripts/controller/CalcController.js
@@ -1,5 +1,26 @@
 class CalcController {
 
+    constructor(){
+        this._displayCalc = "0";
+        this._currentDate;
+        // uso de underline (_) aplica-se em atributos privados, para controlar o acesso
+    }
 
+    get displayCalc(){
+        return this._displayCalc;
+    }
+
+    set displayCalc(value){
+        this._displayCalc = value;
+    }
+
+    get dataAtual(){
+        return this._currentDate;
+    }
+
+    // set não precisa de return apenas atribuição do valor <---------------------
+    set dataAtual(value){
+        this._currentDate = value;  
+    }
 
 }
