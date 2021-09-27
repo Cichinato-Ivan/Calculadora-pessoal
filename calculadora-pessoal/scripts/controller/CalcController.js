1cde2a603b24c25b4babba3f25171d62d2f7cf29
Ivan Cichinato de Oliveira <ivan.oliveira01@yahoo.com>
2021-09-27 21:53:12 +0100

    manipulando Dom

--- calculadora-pessoal/scripts/controller/CalcController.js
+++ calculadora-pessoal/scripts/controller/CalcController.js
@@ -1,7 +1,9 @@
 class CalcController {
 
     constructor(){
-        this._displayCalc = "0";
+        this._displayCalcEl = document.querySelector("#display");
+        this._dateEl = document.querySelector("#data");
+        this._timeEl = document.querySelector("#hora");
         this._currentDate;
         // uso de underline (_) aplica-se em atributos privados, para controlar o acesso
         this.initialize();
@@ -9,26 +11,43 @@ class CalcController {
 
     initialize(){
 
-        let displayCalcEl = doucment.querySelector("#display");
-        let dateEl = docment.querySelector("#data");
-        let timeEl = document.querySelector("#hora");
+        this._dateEl.innerHTML = "27/09/21";
+        this._timeEl.innerHTML = "21:30";
 
     }
 
+    get displayDate(){
+        return this._dateEl.innerHTML;
+    }
+    
+    set displayDate(value){
+        this._dateEl.innerHTML = value;
+    }
+
+    get displayTime(){
+        return this._timeEl.innerHTML;
+    }
+
+    set displayTime(value){
+        this._timeEl.innerHTML = value;
+    }
+
+    
+
     get displayCalc(){
-        return this._displayCalc;
+        return this._displayCalcEl.innerHTML;
     }
 
     set displayCalc(value){
-        this._displayCalc = value;
+        this._displayCalcEl.innerHTML = value;
     }
 
-    get dataAtual(){
-        return this._currentDate;
+    get currentDate(){
+        return new Date();
     }
 
     // set não precisa de return apenas atribuição do valor <---------------------
-    set dataAtual(value){
+    set currentDate(value){
         this._currentDate = value;  
     }
 
