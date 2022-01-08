49082e83fdbfe0d57323e8035c54a0fa3d84eb79
Ivan Cichinato de Oliveira <ivan.oliveira01@yahoo.com>
2022-01-07 23:30:46 -0300

    método construtor e encapsulamento

--- calculadora-pessoal/scripts/controller/CalcController.js
+++ calculadora-pessoal/scripts/controller/CalcController.js
@@ -1,5 +1,41 @@
 class CalcController {
 
-    
+    constructor(){
+
+        this._displayCalc = "0";
+        this._currentDate;
+        this.initialize();
+
+    }
+
+    initialize(){
+
+
+
+    }
+
+    get displayCalc(){
+
+        return this._displayCalc;
+
+    }
+
+    set displayCalc(valor){
+
+        this._displayCalc = valor;
+
+    }
+
+    get currentDate(){
+
+        return this._dataAtual;
+
+    }
+
+    set currentDate(valor){
+
+        this._currentDate = valor;
+
+    }
 
 }
