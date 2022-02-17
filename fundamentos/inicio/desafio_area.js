86bff4887ac8f60a0b587a859b6698565eb8ba67
Cichinato Ivan <ivanoliveira712@gmail.com>
2022-02-17 00:23:57 -0300

    desafios

---
+++ fundamentos/inicio/desafio_area.js
@@ -0,0 +1,8 @@
+// PI = 3.141592
+// PI * raio * raio
+
+const PI = 3.141592
+let raio = 10
+let area = PI * raio * raio
+
+console.log('A área da circunferência de raio 10 é: ' + area + ' m².')
---
+++ fundamentos/inicio/desafio_troca.js
@@ -0,0 +1,14 @@
+let a = 7
+let b = 94
+let c 
+
+console.log('a = ' + a + ', b = ' + b)
+
+c = a 
+a = b
+b = c
+
+console.log('a = ' + a + ', b = ' + b)
+
+
+// depois da troca ... a = 94 e b = 7
