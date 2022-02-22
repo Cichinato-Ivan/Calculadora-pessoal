bac698f2e9b4eaff7d9bf51b1161b75f1d6f8bb3
Cichinato Ivan <ivanoliveira712@gmail.com>
2022-02-21 23:07:39 -0300

    finalizando com strings

--- fundamentos/Exercicios-js/fundamentos/string.js
+++ fundamentos/Exercicios-js/fundamentos/string.js
@@ -1,5 +1,14 @@
-const raio = 5.6
-const area = Math.PI * Math.pow(raio, 2)
+const escola = "Cod3r"
 
-console.log(area.toFixed(2))
-console.log(typeof Math)
+console.log(escola.charAt(4))
+console.log(escola.charAt(5))
+console.log(escola.charCodeAt(3))
+console.log(escola.indexOf('3'))
+
+console.log(escola.substring(1))
+console.log(escola.substring(0, 3))
+
+console.log('Escola '.concat(escola).concat ("!"))
+console.log(escola.replace(3, 'e'))
+
+console.log('Ana,Maria,Pedro'.split(','))
