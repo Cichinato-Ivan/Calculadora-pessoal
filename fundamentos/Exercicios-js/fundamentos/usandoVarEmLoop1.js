c4f5407929763f959781e4b4ead6b03ee22b4e37
Ivan Cichinato de Oliveira <87286885+Cichinato-Ivan@users.noreply.github.com>
2022-03-18 23:53:59 -0300

    Estudo
    Estudando os fundamentos de javascript

---
+++ fundamentos/Exercicios-js/fundamentos/usandoVarEmLoop1.js
@@ -0,0 +1,7 @@
+for (var i = 0; i < 10; i++) {
+    console.log(i)
+}
+
+console.log('i = ', i)
+
+// mesmo em um laço de repetiçãp a variável declarada como VAR ela abrange no escopo global, isso pode gerar problerma ou bug no sistema
