a3fb8f4901fe2615524d8a574bd4d6fbc5808b85
Ivan Cichinato de Oliveira <ivan.oliveira01@yahoo.com>
2022-07-04 02:15:26 -0300

    FlatMap, Concat

---
+++ array/concat.js
@@ -0,0 +1,6 @@
+const filhas = ['Valeska', 'Cibelena']
+const filhos = ['Uoxiton', 'Vesclei']
+const todos = filhas.concat(filhos)
+console.log(todos, filhas, filhos);
+
+console.log([].concat([1, 2], [3, 4], 5, [[6, 7]]))
