60ed1931df7a40c2d83b6d542f5136696a721a51
Ivan Cichinato de Oliveira <ivan.oliveira01@yahoo.com>
2022-07-09 02:27:06 -0300

    finalizando capitulo ESNEXT

--- esnext/promise5.js
+++ esnext/promise5.js
@@ -2,14 +2,24 @@ const { reject } = require("lodash")
 
 function funcionarOuNao(valor, chanceErro) {
     return new Promise ((resolve, rejct) => {
-        if(Math.random() < chanceErro) {
-            reject('Ocorreu um erro')
-        } else {
-            resolve(valor)
+        try {
+            if(Math.random() < chanceErro) {
+                reject('Ocorreu um erro')
+            } else {
+                resolve(valor)
+            }
+        } catch (e) {
+            reject(e)
         }
     })
 }
 
-funcionarOuNao('Testando...', 0.9)
+funcionarOuNao('Testando...', 0.5)
     .then(v => console.log(`Valor: ${v}`))
+    .then(
+        v => console.log(v),
+        err => console.log(`Erro esp.: ${err}`)
+    )
+    .then(() => console.log('Qase Fim'))
     .catch(err => console.log(`Erro: ${err}`))
+    .then(() => console.log('Fim'))
