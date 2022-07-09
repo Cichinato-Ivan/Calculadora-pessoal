0437c6c9362283c8c5858931e740d54ace81e21d
Ivan Cichinato de Oliveira <ivan.oliveira01@yahoo.com>
2022-07-09 03:20:43 -0300

    iniciando com conceitos WEB

---
+++ Js/fundamentos/Exercicios-js/fundamentos/usandoVar1.js
@@ -0,0 +1,17 @@
+{
+    {
+        {
+            { 
+                var sera = 'Será???'
+            }
+        }
+    }
+}
+console.log(sera)
+
+function test() {
+    var local = 123 // Uma Variável declarada como VAR não executa fora de uma função
+}
+
+teste()
+console.log(local)
--- fundamentos/Exercicios-js/fundamentos/usandoVarEmLoop1.js
+++
@@ -1,7 +0,0 @@
-for (var i = 0; i < 10; i++) {
-    console.log(i)
-}
-
-console.log('i = ', i)
-
-// mesmo em um laço de repetiçãp a variável declarada como VAR ela abrange no escopo global, isso pode gerar problerma ou bug no sistema
