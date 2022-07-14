d67b4161107e46ed97201b6b6dce5ddf28625def
Ivan Cichinato de Oliveira <ivan.oliveira01@yahoo.com>
2022-07-14 05:06:51 -0300

    resolvendo problemas matameticos em js

--- exercicios/lista_exercicios_1/minhasRespostas/exer_1_10.js
+++ exercicios/lista_exercicios_1/minhasRespostas/exer_1_10.js
@@ -1,5 +1,7 @@
 // Exercício 1 ________________________
 
+const { functions } = require("lodash")
+
 // 01) Crie uma função que dado dois valores (passados como parâmetros) mostre no console a soma, subtração, multiplicação e divisão desses valores
 
 function calc(num1, num2) {
@@ -73,3 +75,42 @@ function money(valorDecimal) { // v = valor
    console.log(valorEmReais)
 }
 money(0.4 + 1.2)
+
+// __________________________________________________________
+/*
+06) Elabore duas funções que recebem três parâmetros: capital inicial, taxa de juros e tempo de aplicação. A primeira função retornará o montante da aplicação financeira sob o regime de juros simples e a segunda
+retornará o valor da aplicação sob o regime de juros compostos.
+*/
+
+function jurosSimples (capitalInicial, taxa, tempo) {
+    return capitalInicial + (capitalInicial * taxa * tempo)
+}
+
+function jurosCompostos (capitalInicial, taxa, tempo) {
+    return capitalInicial * (1 + taxa) ** tempo
+}
+
+console.log(jurosSimples(100, 10/100, 2));
+console.log(jurosCompostos(100, 10/100, 2));
+
+// ____________________________________________________________________
+/*
+07) Uma das vantagens da programação é a automatização de tarefas que não gostamos de realizar. Dito isto,
+elabore uma função cujo objetivo é resolver a fórmula de Bhaskara. Para isso, sua função deve receber três
+parâmetros, “ax2”, “bx” e “c”, de tal modo que na equação: 3x² - 5x + 12 os valores seriam respectivamente: 3, -5, 12. Como retorno deve ser passado um vetor que tem 2 valores um para cada possível resultado, mesmo que os resultados sejam iguais. Caso o delta seja negativo, retorne, ao invés do vetor, um string com a frase: “Delta é negativo”.
+*/
+function bhaskara (ax2, bx, c) {
+    let resultados = []
+    let delta = (bx ** 2) - (4 * ax2 * c)
+    if(delta < 0){
+        return "Delta é negativo"
+    } 
+    let x1 = (-bx + Math.sqrt(delta))/(2*ax2)
+    let x2 = (-bx - Math.sqrt(delta))/(2*ax2)
+    resultados.push(x1)
+    resultados.push(x2)
+    return resultados   
+}
+
+console.log(bhaskara(1, 3, 2))
+console.log(bhaskara(3, 1, 2))
